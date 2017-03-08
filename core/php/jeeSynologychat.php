071cd6c7a0f70821850eecaf40c08e303e8871d0
Loïc <loic@jeedom.com>
2017-03-08 17:08:24 +0100

    Add compatibility with send capture

--- core/php/jeeSynologychat.php
+++ core/php/jeeSynologychat.php
@@ -44,5 +44,16 @@ $cmd_text->event(trim(init('text')));
 $cmd_sender = $eqLogic->getCmd('info', 'sender');
 $cmd_sender->event(init('username'));
 $reply = interactQuery::tryToReply(trim(init('text')), $parameters);
-echo json_encode(array('text' => $reply));
+if (!is_array($reply)) {
+	$reply = array('reply' => $reply);
+}
+if (isset($reply['file']) && count($reply['file']) > 0) {
+	if (!is_array($reply['file'])) {
+		$reply['file'] = array($reply['file']);
+	}
+	$send = $eqLogic->getCmd(null, 'send');
+	$send->execCmd(array('files' => $reply['file'], 'message' => $reply['reply'], 'title' => ''));
+	die();
+}
+echo json_encode(array('text' => $reply['reply']));
 ?>
