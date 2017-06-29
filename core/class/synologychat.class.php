59db1e14770d70a69df7ce5e660d5f335803aaf4
Loïc <loic@jeedom.com>
2017-06-29 15:55:48 +0200

    prevent message too soon

--- core/class/synologychat.class.php
+++ core/class/synologychat.class.php
@@ -101,9 +101,10 @@ class synologychatCmd extends cmd {
 			if (!isset($decode_result['success']) || !$decode_result['success']) {
 				throw new Exception(__('Erreur : ', __FILE__) . $result);
 			}
+			usleep(100);
 			if (isset($_options['files']) && count($_options['files']) > 0) {
 				foreach ($_options['files'] as $file) {
-					sleep(1);
+
 					$post = array('file_url' => network::getNetworkAccess($eqLogic->getConfiguration('networkmode')) . '/plugins/synologychat/core/php/jeeFile.php?apikey=' . jeedom::getApiKey('synologychat') . '&file=' . urlencode($file));
 					$payload = str_replace('&', '%26', json_encode($post));
 					$request_http->setPost('payload=' . $payload);
@@ -115,6 +116,7 @@ class synologychatCmd extends cmd {
 					if (!isset($decode_result['success']) || !$decode_result['success']) {
 						throw new Exception(__('Erreur : ', __FILE__) . $result);
 					}
+					sleep(100);
 				}
 			}
 		}
